abb41434ef2fb61d1ea7935749fa5c8b8ac28b99
MWypych <wypych.mic@gmail.com>
2024-10-08 12:50:14 +0200

    make the map

--- source.R
+++ source.R
@@ -92,3 +92,53 @@ for(i in parknames) {
 
 ## making a map of National Parks
 
+park_locations <- data.frame(name = parknames,
+                             lat = c(44.338974,
+                                     37.593048,
+                                     41.3733,
+                                     48.7596,
+                                     36.056595,
+                                     43.790802,
+                                     35.611763,
+                                     34.521530,
+                                     41.6600,
+                                     33.881866,
+                                     47.802109,
+                                     40.343182,
+                                     44.423691,
+                                     37.865101,
+                                     37.297817
+                                     ),
+                             long = c(-68.273430,
+                                      -112.187332,
+                                      -81.6164,
+                                      -113.7870,
+                                      -112.125092,
+                                      -110.684944,
+                                      -83.489548,
+                                      -93.042267,
+                                      -87.0400,
+                                      -115.900650,
+                                      -123.604355,
+                                      -105.688103,
+                                      -110.588516,
+                                      -119.538330,
+                                      -113.028770
+                                      ))
+
+library(maps)
+library(mapdata)
+library(ggrepel)
+state <- map_data("state")
+
+park_map <- state %>% 
+  ggplot(aes(x=long, y=lat, group = group)) + 
+  geom_polygon(color = "white", fill = my_pal_alt[2]) +
+  geom_point(data = park_locations, aes(x = long, y = lat), inherit.aes = FALSE) +
+  geom_label_repel(data = park_locations, aes(x = long, y = lat, label = name), size = 5, fill = NA, family = "Ubuntu", inherit.aes = FALSE,label.size = 0, force_pull = 10) +
+  my_theme() +
+  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) +
+  coord_fixed(1.3)
+
+ggsave("img/parkmap.png",park_map,  width = 8, height = 6)
